0dc400033874fa5ffa2bc20f9af44b57343821ef
Ariele Amorim <arieleamorim@Arieles-MacBook-Pro.local>
2023-04-21 11:22:51 +0200

    done

--- school.py
+++ school.py
@@ -1,15 +1,19 @@
 # pylint:disable=C0111,C0103
+import sqlite3
+conn = sqlite3.connect('data/school.sqlite')
 
 def students_from_city(db, city):
     """return a list of students from a specific city"""
-    pass  # YOUR CODE HERE
-
+    db = conn.cursor()
+    query = """SELECT s.first_name, s.last_name
+    FROM students s
+    WHERE s.birth_city LIKE ?"""
+    db.execute(query, (f"%{city}%",))
+    result = db.fetchall()
+    return [list(i) for i in result]
 
 # To test your code, you can **run it** before running `make`
 #   => Uncomment the following lines + run:
-#   $ python school.py
-#
-# import sqlite3
-# conn = sqlite3.connect('data/school.sqlite')
-# db = conn.cursor()
+#$ python school.py
+
 # print(students_from_city(db, 'Paris'))
